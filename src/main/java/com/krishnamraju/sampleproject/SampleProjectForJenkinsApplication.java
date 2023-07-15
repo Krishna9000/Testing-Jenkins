3376511db40d8093a3e1f4a3cd4f78d5b859b8a0
Rama Krishnam Raju Kalidindi <krishna@Ramas-Mac-mini.attlocal.net>
2023-07-15 16:56:38 -0400

    initial commit

---
+++ src/main/java/com/krishnamraju/sampleproject/SampleProjectForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.krishnamraju.sampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectForJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishnamraju/sampleproject/SampleProjectForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishnamraju.sampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
